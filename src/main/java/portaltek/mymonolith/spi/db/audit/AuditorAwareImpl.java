24ed78b1b46c2476b753a69356cf6c8ad1be34b2
ronald.pineda <rpineda@illuminateed.net>
2021-07-04 12:18:22 -0600

    adding @SuppressWarnings("NullableProblems") to AuditorAwareImpl

--- src/main/java/portaltek/mymonolith/spi/db/audit/AuditorAwareImpl.java
+++ src/main/java/portaltek/mymonolith/spi/db/audit/AuditorAwareImpl.java
@@ -5,10 +5,11 @@ import org.springframework.data.domain.AuditorAware;
 
 import java.util.Optional;
 
-@SuppressWarnings("NullableProblems")
+
 public class AuditorAwareImpl implements AuditorAware<String> {
 
 	@Override
+	@SuppressWarnings("NullableProblems")
 	public Optional<String> getCurrentAuditor() {
 		return Optional.of("Logged User");
 	}
