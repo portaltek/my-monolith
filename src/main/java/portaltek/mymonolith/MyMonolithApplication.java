3227e3006a587075a139a802edfefbcdbbfaa59f
ronald.pineda <rpineda@illuminateed.net>
2021-07-04 10:42:50 -0600

    init

---
+++ src/main/java/portaltek/mymonolith/MyMonolithApplication.java
@@ -0,0 +1,13 @@
+package portaltek.mymonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMonolithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/portaltek/mymonolith/MyMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package portaltek.mymonolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
